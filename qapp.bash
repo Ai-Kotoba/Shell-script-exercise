b30773abf0991e1ec240d151f0a78438a7311bad
AiKotoba <zhou.voyager@gmail.com>
2022-07-09 21:49:25 +0800

    qapp.bash over!

---
+++ qapp.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# quick操作 git add, git commit, git push 三合一
+
+if [[  "$#" -eq 0  ]]; then
+    echo "未指定位置参数!" >&2
+    exit 1
+fi
+for file in "$@";
+do
+    git add "$file"
+done
+git commit -m "qapp.bash over!"
+git push
